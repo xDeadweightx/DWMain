7fb0cdee9bbbf8e200762020b4bd88314aebc777
xDeadweightx <bennettcreations@yahoo.com>
2015-10-12 20:50:39 -0400

    Main

--- DW Main.lua
+++ DW Main.lua
@@ -1,10 +1,12 @@
-local manifest = {version=0.02, special="Nebelwolfi"}
+local manifest = {version=0.03, special="Nebelwolfi"}
 local q = {p=LIB_PATH, s=SCRIPT_PATH}
 local folders = {"DW","DW\\AI","DW\\Bots","DW\\Champions","DW\\Other"}
 
+local mods = {}
+
 local hosts = {
   host = "raw.githubusercontent.com",
-  path = "/xDeadweightx/DWMain/DWMain/DW/Other/",
+  path = "/xDeadweightx/DWMain/DWMain/",
   append = "?no-cache="..math.random(1,25000)
 }
 
@@ -12,11 +14,11 @@ local function checkDIR(name)
   if not DirectoryExist(name) then CreateDirectory(name) end
 end
 
-local function forceDownload(path,name)
+local function forceDownload(path,name,pathLoc)
   name = name..".lua"
   if not FileExist(path..name) then
     PrintChat("Downloading script: "..path..name)
-    local UPDATE_URL = "https://"..hosts.host..hosts.path..name..hosts.append
+    local UPDATE_URL = "https://"..hosts.host..hosts.path..pathLoc..name..hosts.append
     DownloadFile(UPDATE_URL, path..name, function () PrintChat("Successfully Downloaded: "..name) end)
   end
 end
@@ -27,12 +29,21 @@ function OnLoad()
     checkDIR(q.p..value)
   end
   
-  forceDownload(q.p.."DW\\Other\\","DW Updater")
+  forceDownload(q.p.."DW\\Other\\","DW Updater","DW/Other/")
   
-  mods = require "DW\\Other\\DW Updater"
   
-  mods.printInfo("A huge thanks to "..manifest.special.." for all his help and time. I cannot list the amount of things he helped and taught me. Again I would love to thank him. So this is for you "..manifest.special)
+  mods["Updater"] = require "DW\\Other\\DW Updater"
+  mods.Updater.printInfo("A huge thanks to "..manifest.special.." for all his help and time. I cannot list the amount of things he helped and taught me. Again I would love to thank him. So this is for you "..manifest.special)
   
-  mods.updateScript("DW Main", q.s, "", manifest.version)
+  --Check Main
+  mods.Updater.updateScript("DW Main", q.s, "", manifest.version)
+  
+  --Checks Updater
+  mods.Updater.updateScript("DW Updater", q.p.."DW\\Other\\", "DW\\Other\\", mods.Updater.returnVersion())
+  
+  --Check Scripts
+  forceDownload(q.p.."DW\\Other\\","Script Configs","DW/Other/")
+  mods["ScriptConfigs"] = require "DW\\Other\\Script Configs"
+  mods.Updater.updateScript("Script Configs", q.p.."DW\\Other\\", "DW\\Other\\", mods.ScriptConfigs.returnVersion())
   
 end
